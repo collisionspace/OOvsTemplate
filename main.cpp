1320139625d7caafa0f69b1b57cc5542b8b5c5d7
Daniel Slone <slonej3@mymail.nku.edu>
2016-09-22 23:51:13 -0400

    added random generation file

--- main.cpp
+++ main.cpp
@@ -1,29 +1,8 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int randomGenerator(int begin, int end) {
-    int random = rand() % end + begin; //number between begin and end
-    return random;
-}
-string randomString() {
-    int size = randomGenerator(1,4);
-    string randomString = "";
-    char cAlphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-
-    for(int i = 0; i < size; i++) {
-        int randomChar = randomGenerator(0,25);
-        randomString += cAlphabet[randomChar];
-    }
-    return randomString;
-}
+#include "RandomGeneration.h"
 
 int main() {
+    RandomGeneration rand = RandomGeneration();
     srand(time(NULL));
-    cout << randomString() << endl;
+    cout << rand.randomString() << endl;
     return 0;
 }
